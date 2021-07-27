450fab4ff1b45473164c74af7c86bcaba18a059f
prakdesh <prakdesh@yahoo.com>
2021-07-27 19:03:14 +0530

    modified hello world text to hello first modified

--- Webgentle.BookStore/Startup.cs
+++ Webgentle.BookStore/Startup.cs
@@ -32,7 +32,7 @@ namespace Webgentle.BookStore
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello First Modified Code!");
                 });
             });
         }
